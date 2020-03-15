ac6e0092233d935d9218cb3b8c3bc04f743ddcf8
Mohapi27 <cassino.happies@gmail.com>
2020-03-15 22:14:06 +0200

    Add payment model etc

---
+++ src/main/webapp/app/entities/payment/payment-update.component.ts
@@ -0,0 +1,146 @@
+import { Component, OnInit, ElementRef } from '@angular/core';
+import { HttpResponse } from '@angular/common/http';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { JhiDataUtils, JhiFileLoadError, JhiEventManager, JhiEventWithContent } from 'ng-jhipster';
+
+import { IPayment, Payment } from 'app/shared/model/payment.model';
+import { PaymentService } from './payment.service';
+import { AlertError } from 'app/shared/alert/alert-error.model';
+import { IUser } from 'app/core/user/user.model';
+import { UserService } from 'app/core/user/user.service';
+
+@Component({
+  selector: 'jhi-payment-update',
+  templateUrl: './payment-update.component.html'
+})
+export class PaymentUpdateComponent implements OnInit {
+  isSaving = false;
+  users: IUser[] = [];
+  paymentDateDp: any;
+  modifiedDateDp: any;
+
+  editForm = this.fb.group({
+    id: [],
+    paymentDate: [],
+    attachment: [],
+    attachmentContentType: [],
+    status: [],
+    amount: [],
+    reason: [],
+    modifiedDate: [],
+    modifiedBy: [],
+    userId: []
+  });
+
+  constructor(
+    protected dataUtils: JhiDataUtils,
+    protected eventManager: JhiEventManager,
+    protected paymentService: PaymentService,
+    protected userService: UserService,
+    protected elementRef: ElementRef,
+    protected activatedRoute: ActivatedRoute,
+    private fb: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.data.subscribe(({ payment }) => {
+      this.updateForm(payment);
+
+      this.userService.query().subscribe((res: HttpResponse<IUser[]>) => (this.users = res.body || []));
+    });
+  }
+
+  updateForm(payment: IPayment): void {
+    this.editForm.patchValue({
+      id: payment.id,
+      paymentDate: payment.paymentDate,
+      attachment: payment.attachment,
+      attachmentContentType: payment.attachmentContentType,
+      status: payment.status,
+      amount: payment.amount,
+      reason: payment.reason,
+      modifiedDate: payment.modifiedDate,
+      modifiedBy: payment.modifiedBy,
+      userId: payment.userId
+    });
+  }
+
+  byteSize(base64String: string): string {
+    return this.dataUtils.byteSize(base64String);
+  }
+
+  openFile(contentType: string, base64String: string): void {
+    this.dataUtils.openFile(contentType, base64String);
+  }
+
+  setFileData(event: Event, field: string, isImage: boolean): void {
+    this.dataUtils.loadFileToForm(event, this.editForm, field, isImage).subscribe(null, (err: JhiFileLoadError) => {
+      this.eventManager.broadcast(
+        new JhiEventWithContent<AlertError>('kgotlaSocietySchemeApp.error', { message: err.message })
+      );
+    });
+  }
+
+  clearInputImage(field: string, fieldContentType: string, idInput: string): void {
+    this.editForm.patchValue({
+      [field]: null,
+      [fieldContentType]: null
+    });
+    if (this.elementRef && idInput && this.elementRef.nativeElement.querySelector('#' + idInput)) {
+      this.elementRef.nativeElement.querySelector('#' + idInput).value = null;
+    }
+  }
+
+  previousState(): void {
+    window.history.back();
+  }
+
+  save(): void {
+    this.isSaving = true;
+    const payment = this.createFromForm();
+    if (payment.id !== undefined) {
+      this.subscribeToSaveResponse(this.paymentService.update(payment));
+    } else {
+      this.subscribeToSaveResponse(this.paymentService.create(payment));
+    }
+  }
+
+  private createFromForm(): IPayment {
+    return {
+      ...new Payment(),
+      id: this.editForm.get(['id'])!.value,
+      paymentDate: this.editForm.get(['paymentDate'])!.value,
+      attachmentContentType: this.editForm.get(['attachmentContentType'])!.value,
+      attachment: this.editForm.get(['attachment'])!.value,
+      status: this.editForm.get(['status'])!.value,
+      amount: this.editForm.get(['amount'])!.value,
+      reason: this.editForm.get(['reason'])!.value,
+      modifiedDate: this.editForm.get(['modifiedDate'])!.value,
+      modifiedBy: this.editForm.get(['modifiedBy'])!.value,
+      userId: this.editForm.get(['userId'])!.value
+    };
+  }
+
+  protected subscribeToSaveResponse(result: Observable<HttpResponse<IPayment>>): void {
+    result.subscribe(
+      () => this.onSaveSuccess(),
+      () => this.onSaveError()
+    );
+  }
+
+  protected onSaveSuccess(): void {
+    this.isSaving = false;
+    this.previousState();
+  }
+
+  protected onSaveError(): void {
+    this.isSaving = false;
+  }
+
+  trackById(index: number, item: IUser): any {
+    return item.id;
+  }
+}
